7a08f5b550ead08241e6e55852f12b4ac256cfb7
SIGMA1425 <yahasho1425@gmail.com>
2020-03-13 16:47:36 +0900

    授業変更の取得処理を関数にした

--- func.py
+++ func.py
@@ -8,14 +8,17 @@ exam_name = ["前期中間", "前期定期", "後期中間", "後期定期"]
 
 def get_date():
     now = datetime.datetime.now()
-    now -= datetime.timedelta(days=40)
+    now -= datetime.timedelta(days=38)
     date = str(now.month) + "月" + str(now.day) + "日"
     date += "(" + week[now.weekday()] + ")"
     return date
 
 def shape(origin):
-    origin = origin.split("　")
-    dest = origin[1] + "\n" + origin[3] + origin[5] + "\n"
+    if "→" in origin:
+        origin = origin.split("　")
+        dest = origin[1] + "\n" + origin[3] + origin[5] + "\n"
+    else:
+        dest = origin
     return dest
 
 def test_count():
@@ -23,7 +26,7 @@ def test_count():
         data = json.load(fp)
     for i in range(4):
         exam[i] = data[exam[i]]
-    for i in tqdm(range(4)):
+    for i in range(4):
         test_date = datetime.datetime.strptime(exam[i], "%Y年%m月%d日")
         test_date = datetime.date(year=test_date.year, month=test_date.month, day=test_date.day)
         now = datetime.date.today()
@@ -32,3 +35,41 @@ def test_count():
             return {"exam_name": exam_name[i], "days_left": (test_date - now).days}
     
     return
+
+def get_class_change(bs):
+    output = get_date() + "\n"
+    not_change = "現在のところ\n休講・変更の予定はありません．"
+    
+    for p in bs.select("p"):
+        if "現在のところ休講・変更の予定はありません" in p.text:
+            print("授業変更情報は見つかりませんでした．")
+            output += not_change
+            return output
+
+    print("授業変更情報の存在を確認．探索します．")
+    select_td = bs.select("td")
+
+    index = 0
+    for td in select_td:
+        if "科　目　名" in td.text:
+            break
+        else:
+            index += 1
+
+    console_output = ""
+    flag = False
+    for i, td in enumerate(tqdm(select_td[index:])):
+        if get_date() in td.text:
+            if flag is False:
+                output += "<!channel>" + "\n"
+                flag = True
+            output += select_td[i+index].text.replace(get_date(), "") + "\n"
+            output += shape(select_td[i+index+1].text)
+            console_output += select_td[i+index+1].text + "\n"
+
+    print(console_output, end="")
+    if flag is False:
+        output += not_change
+        print("授業変更情報は見つかりませんでした．")
+    
+    return output
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import json
 import requests
 from bs4 import BeautifulSoup
 import slackweb
-from tqdm import tqdm
 import func
 
 # jsonファイルにwebhookURLやらテストの日やら全部入っている
@@ -14,40 +13,8 @@ slack = slackweb.Slack(url=data["test_slack"])
 response = requests.get(data["TNCT_URL"])
 bs = BeautifulSoup(response.content, "html.parser")
 
-output = func.get_date() + "\n"
-not_change = "現在のところ\n休講・変更の予定はありません．"
-
-print("授業変更情報を取得中...")
-for p in bs.select("p"):
-    if "現在のところ休講・変更の予定はありません" in p.text:
-        print("授業変更情報は見つかりませんでした．")
-        output += not_change
-        slack.notify(text=output)
-        exit()
-
-print("授業変更情報の存在を確認．探索します．")
-select_td = bs.select("td")
-
-index = 0
-for td in select_td:
-    if "科　目　名" in td.text:
-        break
-    else:
-        index += 1
-
-flag = False
-for i, td in enumerate(tqdm(select_td[index:])):
-    if func.get_date() in td.text:
-        if flag is False:
-            output += "<!channel>" + "\n"
-            flag = True
-        output += select_td[i+index].text.replace(func.get_date(), "") + "\n"
-        print(select_td[i+index+1].text)
-        output += func.shape(select_td[i+index+1].text)
-
-if flag is False:
-    output += not_change
-    print("授業変更情報は見つかりませんでした．")
+print("授業変更情報を取得します")
+output = func.get_class_change(bs)
 
 print("テストまでの日数を検索します．")
 test_count = func.test_count()
